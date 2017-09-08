64f1b184d11b5819f6e73616a9b71fff2cf4c947
A3vk <adrivankempen@gmail.com>
2017-09-08 09:23:58 +0200

    Questions

--- BattleShip/src/helloWorld.java
+++ BattleShip/src/helloWorld.java
@@ -9,24 +9,24 @@ public class helloWorld {
 		Scanner sc = new Scanner(System.in);
  
         // String input
+        System.out.println("What is your name?");
         String name = sc.nextLine();
  
         // Character input
-        char gender = sc.next().charAt(0);
+        System.out.println("What is your gender?");
+        String gender = sc.nextLine();
  
         // Numerical data input
         // byte, short and float can be read
         // using similar-named functions.
+        System.out.println("What is your age?");
         int age = sc.nextInt();
-        long mobileNo = sc.nextLong();
-        double cgpa = sc.nextDouble();
  
         // Print the values to check if input was correctly obtained.
-        System.out.println("Name: "+name);
-        System.out.println("Gender: "+gender);
-        System.out.println("Age: "+age);
-        System.out.println("Mobile Number: "+mobileNo);
-        System.out.println("CGPA: "+cgpa);
+        System.out.println("Name: " + name);
+        System.out.println("Gender: " + gender);
+        System.out.println("Age: " + age);
+        System.out.println("Hello " + name + ", you are a " + gender + " with the age of " + age + ".");
 	}
 
 }
