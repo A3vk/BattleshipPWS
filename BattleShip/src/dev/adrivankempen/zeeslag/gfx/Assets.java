fcc3414de01b435521376eca8ec2680e086a274e
A3vk <adrivankempen@gmail.com>
2017-09-15 16:32:28 +0200

    Add graphics

---
+++ BattleShip/src/dev/adrivankempen/zeeslag/gfx/Assets.java
@@ -0,0 +1,14 @@
+package dev.adrivankempen.zeeslag.gfx;
+
+import java.awt.image.BufferedImage;
+
+public class Assets {
+	@SuppressWarnings("unused")
+	private static final int width = 40, height = 40;
+	
+	public static BufferedImage shipBN, shipBO, shipBZ, shipBW, shipMH, shipMV, empty;
+	
+	public static void init(BufferedImage sheet) {
+		
+	}
+}
