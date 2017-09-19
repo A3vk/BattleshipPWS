ae715086fb2cf295e5be0b6a72bb274af66e2477
A3vk <adrivankempen@gmail.com>
2017-09-19 17:23:46 +0200

    FPS cap gemaakt

--- BattleShip/src/dev/adrivankempen/zeeslag/game/Bord.java
+++ BattleShip/src/dev/adrivankempen/zeeslag/game/Bord.java
@@ -6,9 +6,7 @@ import dev.adrivankempen.zeeslag.gfx.Assets;
 import dev.adrivankempen.zeeslag.gfx.ImageLoader;
 
 public class Bord {
-	@SuppressWarnings("unused")
-	private Object[][] tiles;
-	@SuppressWarnings("unused")
+	private Object[][] tiles = new Object[10][10];
 	private int x, y;
 	private int rand = 20;
 	private int size = 40;
@@ -19,10 +17,10 @@ public class Bord {
 	}
 	
 	public void render(Graphics g) {
-		g.drawImage(ImageLoader.loadImage("/textures/Bord.png"), 0, 0, null);
-		for(int i = 0; i < 10; i++) {
-			for(int j = 0; j < 10; j++) {
-				g.drawImage(Assets.leeg, (rand + i * size), (rand + j * size), null);
+		g.drawImage(ImageLoader.loadImage("/textures/Bord.png"), x, y, null);
+		for(int i = 0; i < tiles.length; i++) {
+			for(int j = 0; j < tiles.length; j++) {
+				tiles[i][j] = g.drawImage(Assets.leeg, (x + rand + i * size), (y + rand + j * size), null);
 			}
 		}
 	}
--- BattleShip/src/dev/adrivankempen/zeeslag/game/Tile.java
+++ BattleShip/src/dev/adrivankempen/zeeslag/game/Tile.java
@@ -1,5 +1,9 @@
 package dev.adrivankempen.zeeslag.game;
 
+import java.awt.image.BufferedImage;
+
 public class Tile {
-	
+	public Tile(BufferedImage image, int posX, int posY, int x, int y) {
+		
+	}
 }
