d6a6087e222c2c27ca8c523d1b8ba2eede3b2406
A3vk <adrivankempen@gmail.com>
2017-10-04 19:14:20 +0200

    Preview gemaakt Begin aan opschonen / verbeteren voor ALPHA 1

--- BattleShip/src/dev/adrivankempen/zeeslag/game/bord/SpelerBord.java
+++ BattleShip/src/dev/adrivankempen/zeeslag/game/bord/SpelerBord.java
@@ -4,13 +4,9 @@ import dev.adrivankempen.zeeslag.Handler;
 import dev.adrivankempen.zeeslag.game.ship.Fregat;
 import dev.adrivankempen.zeeslag.game.ship.Kruiser;
 import dev.adrivankempen.zeeslag.game.ship.Mijnveger;
-import dev.adrivankempen.zeeslag.game.ship.Ship;
 import dev.adrivankempen.zeeslag.game.ship.Slagschip;
 
-public class SpelerBord extends Bord {
-	//LOOK FOR FIX --> CODE WERKT NU ZOEK, misschien zoeken naar andere oplossing
-	private final Ship ship = new Ship(0, 0, 0, 0, this);
-	
+public class SpelerBord extends Bord {	
 	private Slagschip slagschip;
 	private Kruiser kruiser;
 	private Fregat fregat1, fregat2;
@@ -23,6 +19,12 @@ public class SpelerBord extends Bord {
 	
 	public SpelerBord(int x, int y, Handler handler) {
 		super(x, y, handler);
+		
+		slagschip = new Slagschip(0, -1, -1, this);
+		kruiser = new Kruiser(0, -1, -1, this);
+		fregat1 = new Fregat(0, -1, -1, this);
+		fregat2 = new Fregat(0, -1, -1, this);
+		mijnveger = new Mijnveger(0, -1, -1, this);
 		
 		this.handler = handler;
 	}
@@ -41,40 +43,55 @@ public class SpelerBord extends Bord {
 			handler.getMouseManager().setLeftPressed();
 			place();
 		}
+		preview();
 	}
 	
 	private void place() {
 		if(click() != null) {
 			if(currentIndex == 0){
-				if(ship.checkPosition(direction, 5, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this)) {
+				if(slagschip.checkPosition(direction, 5, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this)) {
 					slagschip = new Slagschip(direction, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this);
-					System.out.println("PLACED: " + currentIndex);
 					currentIndex++;
 				}
 			} else if(currentIndex == 1) {
-				if(ship.checkPosition(direction, 4, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this)) {
+				if(kruiser.checkPosition(direction, 4, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this)) {
 					kruiser = new Kruiser(direction, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this);
 					currentIndex++;
 				}
 			} else if(currentIndex == 2) {
-				if(ship.checkPosition(direction, 3, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this)) {
+				if(fregat1.checkPosition(direction, 3, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this)) {
 					fregat1 = new Fregat(direction, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this);
 					currentIndex++;
 				}
 			} else if(currentIndex == 3) {
-				if(ship.checkPosition(direction, 3, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this)) {
+				if(fregat2.checkPosition(direction, 3, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this)) {
 					fregat2 = new Fregat(direction, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this);
 					currentIndex++;
 				}
 			} else if(currentIndex == 4) {
-				if(ship.checkPosition(direction, 2, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this)) {
+				if(mijnveger.checkPosition(direction, 2, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this)) {
 					mijnveger = new Mijnveger(direction, getTile(click()).getCoorX(), getTile(click()).getCoorY(), this);
 					currentIndex++;
+					System.out.println("ALL PLACED");
 				}
-			} else {
-				System.out.println("ALL PLACED");
 			}
 		}
+	}
+	
+	private void preview() {
+		try {
+			if(currentIndex == 0){
+				slagschip.preview(direction, getTile(click()).getCoorX(), getTile(click()).getCoorY());
+			} else if(currentIndex == 1) {
+				kruiser.preview(direction, getTile(click()).getCoorX(), getTile(click()).getCoorY());
+			} else if(currentIndex == 2) {
+				fregat1.preview(direction, getTile(click()).getCoorX(), getTile(click()).getCoorY());
+			} else if(currentIndex == 3) {
+				fregat2.preview(direction, getTile(click()).getCoorX(), getTile(click()).getCoorY());
+			} else if(currentIndex == 4) {
+				mijnveger.preview(direction, getTile(click()).getCoorX(), getTile(click()).getCoorY());
+			}
+		} catch(NullPointerException e){}
 	}
 
 	//GETTERS EN SETTERS
